0e5c4a462905e55eb57efec6f7d33348c3290e9f
delgadoarlee <delgadoarlee@gmail.com>
2024-01-23 20:31:54 +0800

    Initialize jest

--- App.test.tsx
+++ App.test.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable prettier/prettier */
 // Written to bypass jest fail, will fix later
-describe("<App />", () => {
-  it("to Pass", () => {
+describe("<App/>", () => {
+  it("will pass", () => {
     const tree = 1;
     expect(tree).toBeDefined();
   });
