3659414b2062f1ed18e29715aaec5f57c3e41f47
delgadoarlee <delgadoarlee@gmail.com>
2024-01-24 18:45:32 +0800

    Added navigation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Home from "./src/screens/Home";
+
+import Navigator from "./src/Navigation";
 
 export default function App() {
-  return (
-    <Home/>
-  )
+  return <Navigator />;
 }
-
-
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,42 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+// import { StatusBar } from "expo-status-bar";
+// import React from "react";
+// import { StyleSheet, Text, View } from "react-native";
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Booking from "../Booking";
+import Chat from "../Chat";
+import Settings from "../Settings";
+
+const Tab = createBottomTabNavigator();
+
+const Home = () =>  {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Booking" component={Booking} options={{headerShown: false}} />
+      <Tab.Screen name="Chat" component={Chat} options={{headerShown: false}} />
+      <Tab.Screen name="Settings" component={Settings} options={{headerShown: false}} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
+export default Home;
