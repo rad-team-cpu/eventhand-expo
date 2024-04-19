04598080e7586200d1e56b0a4a46a88e88b790ea
delgadoarlee <delgadoarlee@gmail.com>
2024-04-19 21:56:45 +0800

    Updated App.tsx

--- App.tsx
+++ App.tsx
@@ -3,16 +3,14 @@ import Constants from "expo-constants";
 import React from "react";
 
 import Navigator from "./src/Navigation";
-import SignupForm from "./src/screens/SignUp";
+
 
 export default function App() {
-  console.log(Constants.expoConfig?.extra?.clerkPublishableKey);
   return (
     <ClerkProvider
       publishableKey={Constants.expoConfig?.extra?.clerkPublishableKey}
     >
-      {/* <Navigator /> */}
-      <SignupForm/>
+      <Navigator />
     </ClerkProvider>
   );
 }
