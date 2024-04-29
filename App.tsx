ff1b9e83bcbef5c07077dd427cf852ed3b897d0a
delgadoarlee <delgadoarlee@gmail.com>
2024-04-29 14:32:31 +0800

    Added token cache for storing key-value pairs

--- App.tsx
+++ App.tsx
@@ -1,13 +1,31 @@
 import { ClerkProvider } from "@clerk/clerk-expo";
 import Constants from "expo-constants";
+import * as SecureStore from "expo-secure-store";
 import React from "react";
 
 import Navigator from "./src/Navigation";
 
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export default function App() {
   return (
     <ClerkProvider
+      tokenCache={tokenCache}
       publishableKey={Constants.expoConfig?.extra?.clerkPublishableKey}
     >
       <Navigator />
