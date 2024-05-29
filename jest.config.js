f614421b97a42305461e25431ba898ce9ea1638d
delgadoarlee <delgadoarlee@gmail.com>
2024-05-29 17:04:32 +0800

    Combined jest-test and setup.ts

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
       'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|firebase|@firebase/.*)'
     ],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    setupFilesAfterEnv: ["./test/jest-test.ts", "./test/setup.ts"],
+    setupFilesAfterEnv: ["./test/setup.ts"],
     setupFiles: [
       "./test/mocks.ts",
     ],
