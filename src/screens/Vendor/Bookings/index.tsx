5e9b581f8a591abc60fd7e9f727076a8a2a2ed90
ashrabino <rasheencrabino@gmail.com>
2024-08-23 21:24:23 +0800

    feat: display of bookings of vendors

--- src/screens/Vendor/Bookings/index.tsx
+++ src/screens/Vendor/Bookings/index.tsx
@@ -1,187 +1,250 @@
-import { useNavigation } from '@react-navigation/native';
+import { FontAwesome } from '@expo/vector-icons';
+import axios from 'axios';
 import { format } from 'date-fns/format';
-import React, { useContext, useMemo } from 'react';
-import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
-import Block from 'Components/Ui/Block';
+import Entypo from '@expo/vector-icons/Entypo';
+import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
+import React, { useEffect, useState } from 'react';
 import Image from 'Components/Ui/Image';
-import useTheme from '../../../core/theme';
-import { StatusBar } from 'expo-status-bar';
+import { Alert, Pressable, StyleSheet, Text, View } from 'react-native';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import useTheme from 'src/core/theme';
 import {
+  BookingViewScreenProps,
+  ScreenProps,
+  BookingStatus,
   BookingDetailsProps,
-  EventInfo,
+  PackageType,
+  Product,
+  HomeScreenBottomTabsProps,
   HomeScreenNavigationProp,
 } from 'types/types';
-import { VendorContext } from 'Contexts/VendorContext';
+import Button from 'Components/Ui/Button';
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
-const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
+function BookingView({ route, navigation }: BookingViewScreenProps) {
+  const { _id } = route.params;
+  const { colors, sizes } = useTheme();
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: 'confirmed', title: 'Confirmed' },
+    { key: 'pending', title: 'Pending' },
+  ]);
+  const [booking, setBooking] = useState<BookingDetailsProps>();
 
-const BookingListItem = ({
-  _id,
-  client,
-  event,
-}: BookingDetailsProps) => {
-  const borderColor = useMemo(() => getRandomColor(), []);
-  const navigation = useNavigation<HomeScreenNavigationProp>();
+  const fetchBooking = async (_id: string) => {
+    try {
+      const response = await axios.get(
+        `${process.env.EXPO_PUBLIC_BACKEND_URL}/booking?_id=${_id}`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      setBooking(response.data);
+    } catch (error: any) {
+      if (error instanceof TypeError) {
+        console.error(
+          'Network request failed. Possible causes: CORS issues, network issues, or incorrect URL.'
+        );
+      } else {
+        console.error('Error fetching booking:', error.message);
+      }
+    }
+  };
 
-  // const onPress = () =>
-  //   navigation.navigate('BookingView', {
-  //     _id,
-  //     packages,
-  //     client,
-  //     event,
-  //   }
-  // );
+  // const handleFindSupplier = () => {
+  //   navigation.navigate('Home', { initialTab: 'Vendors' });
+  // };
 
-  return (
-    <Pressable
-      key={_id}
-      style={[styles.itemContainer, { borderLeftColor: borderColor }]}
-      android_ripple={{ color: '#c0c0c0' }}
-      // onPress={onPress}
-    >
-      <Text style={styles.dateText}>{event?.name}</Text>
-      <View style={styles.separator} />
-      <View style={styles.row}>
-        {/* <Text style={styles.dateText}>{?._id}</Text> */}
-        <Text style={styles.capacityText}>
-          <Text style={styles.dateText}>
-            {client?.firstName}' '{client?.firstName}
-          </Text>
-        </Text>
-      </View>
-    </Pressable>
-  );
-};
-
-interface BookingsProps {
-  bookings: BookingDetailsProps[];
-}
-
-const Bookings = ({ bookings }: BookingsProps) => (
-  <FlatList
-    contentContainerStyle={styles.listContainer}
-    data={bookings}
-    renderItem={({ item }) => (
-      <BookingListItem
-        _id={item._id}
-        package={item.package}
-        client={item.client}
-        event={item.event}
-      />
-    )}
-  />
-);
-
-function BookingList() {
-  const vendorContext = useContext(VendorContext);
-  const { assets, colors, sizes, gradients } = useTheme();
-
-  const navigation = useNavigation<HomeScreenNavigationProp>();
-
-  if (!vendorContext) {
-    throw new Error('UserInfo must be used within a UserProvider');
-  }
-
-  const { vendor } = vendorContext;
-  const { bookings } = vendor;
-
-  if (bookings && bookings.length > 0) {
-    return (
-      <Block safe>
-        <StatusBar style='auto' />
-        <Block flex={0} style={{ zIndex: 0 }}>
-          <Text className='pt-10 pl-6 font-bold text-2xl text-pink-600'>
-            Upcoming Bookings
-          </Text>
-          <Bookings bookings={bookings} />
-        </Block>
-      </Block>
+  const handleDeclineBooking = (id: string) => {
+    Alert.alert(
+      'Confirm Decline',
+      'Are you sure you want to decline this request to book?',
+      [
+        {
+          text: 'NO',
+          style: 'cancel',
+        },
+        {
+          text: 'YES',
+          onPress: async () => {
+            try {
+              await axios.patch(
+                `${process.env.EXPO_PUBLIC_BACKEND_URL}/booking/${id}`,
+                {
+                  bookingStatus: 'Cancelled', // Add bookingStatus to the request body
+                },
+                {
+                  headers: {
+                    'Content-Type': 'application/json',
+                  },
+                }
+              );
+              fetchBooking(_id);
+            } catch (error: any) {
+              console.error('Error declining booking:', error.message);
+            }
+          },
+        },
+      ]
     );
-  }
+  };
+
+  useEffect(() => {
+    fetchBooking(_id);
+  }, []);
 
   return (
-    <Block safe>
-      <View testID='test-events' style={styles.container}>
-        <Image
-          background
-          resizeMode='cover'
-          padding={sizes.md}
-          source={assets.noEvents}
-          rounded
-          className='rounded-xl h-72 w-72'
-        ></Image>
-        <Text className='font-bold'>You have no bookings!</Text>
+    <>
+      <ExpoStatusBar />
+      <View style={listStyles.eventContainer}>
+        <View className='flex flex-row justify-between'>
+          <Button
+            row
+            flex={0}
+            justify='flex-start'
+            onPress={() => navigation.goBack()}
+          >
+            <AntDesign name='back' size={24} color='#CB0C9F' />
+            <Text className='text-primary ml-1'>Go back</Text>
+          </Button>
+          {/* <View style={styles.container}>
+            <Pressable
+              style={styles.button}
+              android_ripple={{ color: '#c0c0c0' }}
+              onPress={() => handleFindSupplier()}
+            >
+              <FontAwesome
+                name='search'
+                size={10}
+                color='white'
+                style={styles.icon}
+              />
+              <Text style={styles.buttonText}>Find Supplier</Text>
+            </Pressable>
+          </View> */}
+        </View>
+
+        {/* <Text style={listStyles.dateText}>{dateString}</Text> */}
+        <View style={listStyles.separator} />
+        <View style={listStyles.row}>
+          {/* <Text style={listStyles.budgetText}>
+            Budget: {budget !== 0 ? `₱${budget}` : '∞'}
+          </Text> */}
+          {/* <Text style={listStyles.capacityText}>
+            Capacity: {attendees !== 0 ? `${attendees}` : '∞'}
+          </Text> */}
+        </View>
       </View>
-    </Block>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginVertical: 1,
   },
   button: {
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: '#6200EE',
     paddingVertical: 10,
-    paddingHorizontal: 20,
+    paddingHorizontal: 10,
     borderRadius: 5,
   },
   icon: {
-    marginRight: 10,
+    marginRight: 5,
   },
   buttonText: {
     color: 'white',
-    fontSize: 16,
-  },
-  floatingBtnContainer: {
-    position: 'absolute',
-    bottom: 10,
-    right: 10,
-  },
-  floatingbutton: {
-    width: 50,
-    height: 50,
-    borderRadius: 15,
-    backgroundColor: '#6200EE',
-    justifyContent: 'center',
-    alignItems: 'center',
-    elevation: 5,
+    fontSize: 12,
   },
   listContainer: {
-    paddingVertical: 16,
-    paddingHorizontal: 30,
-  },
-  itemContainer: {
     padding: 16,
-    marginVertical: 3,
-    marginLeft: 1,
+    marginTop: 5,
+  },
+  roundedContainer: {
+    borderRadius: 10,
     backgroundColor: '#fff',
-    borderLeftWidth: 10,
-    borderTopLeftRadius: 8,
-    borderBottomLeftRadius: 8,
-    borderRightColor: '#fff',
-    borderRightWidth: 5,
-    borderTopRightRadius: 8,
-    borderBottomRightRadius: 8,
-    elevation: 2, // Add shadow for floating effect
+    overflow: 'hidden',
+    elevation: 5,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 4,
   },
+  vendorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+    padding: 10,
+    position: 'relative',
+  },
+  floatingRemoveButton: {
+    position: 'absolute',
+    top: -10,
+    right: -10,
+    backgroundColor: 'white',
+    borderRadius: 50,
+    padding: 1,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+  },
+  vendorLogo: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 16,
+  },
+  vendorName: {
+    fontSize: 15,
+  },
+  tabBar: {
+    backgroundColor: '#fff',
+    marginTop: 5, // Add margin top for TabBar
+    marginHorizontal: 6,
+    elevation: 4, // Optional shadow for TabBar on Android
+    shadowColor: '#000', // Optional shadow for TabBar on iOS
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  indicator: {
+    backgroundColor: '#CB0C9F',
+  },
+  label: {
+    color: '#CB0C9F',
+  },
+});
+
+const listStyles = StyleSheet.create({
+  eventContainer: {
+    paddingHorizontal: 16,
+    paddingVertical: 10,
+    marginTop: 30,
+    marginHorizontal: 5,
+    backgroundColor: '#fff',
+    borderLeftWidth: 8,
+    borderTopLeftRadius: 16,
+    borderBottomLeftRadius: 16,
+    borderLeftColor: '#CB0C9F',
+    borderRightWidth: 8,
+    borderTopRightRadius: 16,
+    borderBottomRightRadius: 16,
+    borderRightColor: '#CB0C9F',
+    elevation: 10, // Add shadow for floating effect
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+  },
   dateText: {
-    fontSize: 16,
+    fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 8,
   },
@@ -195,11 +258,11 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   budgetText: {
-    fontSize: 14,
+    fontSize: 16,
   },
   capacityText: {
-    fontSize: 14,
+    fontSize: 16,
   },
 });
 
-export default BookingList;
+export default BookingView;
