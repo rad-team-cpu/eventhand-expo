1f4f7d17fcc16beed910dc0c817064f421c61f9d
delgadoarlee <delgadoarlee@gmail.com>
2024-09-16 13:13:28 +0800

    Add code to trigger git actions

--- src/core/helpers.tsx
+++ src/core/helpers.tsx
@@ -3,3 +3,7 @@ export default function formatDate(date: any) {
     return dateObj.toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' }) + ' ' +
            dateObj.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' });
   }
+
+  const forPull = () => {
+    
+  }
